5b37b1a11eb3dcf429f4a5c6f762f5887070ab2f
zain <yuepeng63@163.com>
2026-08-07 16:41:05 +0800

    geo文档

--- ruoyi-admin/src/main/java/org/dromara/DromaraApplication.java
+++ ruoyi-admin/src/main/java/org/dromara/DromaraApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.context.metrics.buffering.BufferingApplicationSt
  * @author Lion Li
  */
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.dromara","com.ulpon"})
 public class DromaraApplication {
 
     /**
---
+++ ruoyi-modules/ulpon-geo/src/main/java/com/ulpon/geo/config/GeoConfig.java
@@ -0,0 +1,10 @@
+package com.ulpon.geo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class GeoConfig {
+
+}
